2968a3e6bc1a3ca1250f81704c83c914f5831959
Mind <nathapat.mind@gmail.com>
2023-11-24 00:12:34 +0700

    created method for regrade (full score pls)

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,8 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter number member: ");
-        int n = s.nextInt();
-        if(n<=0){
-            System.out.println("Out of member");
-            return;
-        }
-        int arr[] = new int[n];
+
+    public static int[] sortingArray(int n, int[] arr){
         int temp = 0;
-        System.out.print("Enter all member you want: ");
-        for(int i=0; i<n ; i++){
-            arr[i] = s.nextInt();
-        }
-        System.out.println("This is member before Sorting: ");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
         for(int i=0; i<n;i++){
             for(int j=i+1; j<n;j++){
                 if(arr[i]>arr[j]) {
@@ -28,6 +12,28 @@ public class Main {
                 }
             }
         }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number member: ");
+        int n = s.nextInt();
+        if(n<=0){
+            System.out.println("Out of member");
+            return;
+        }
+        int arr[] = new int[n];
+        System.out.print("Enter all member you want: ");
+        for(int i=0; i<n ; i++){
+            arr[i] = s.nextInt();
+        }
+        System.out.println("This is member before Sorting: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        sortingArray(n,arr);
+        System.out.println();
+
         System.out.println("This is number after sorting: ");
         for(int i=0; i<arr.length;i++){
             System.out.print(arr[i]+" ");
